6952ac74177a8b369252fd8ef765c3983b796b23
Kent Eudy <keudy@vizuri.com>
2018-01-03 09:08:05 -0500

    Init

---
+++ fhir-framinghamRules/run.sh
@@ -0,0 +1,1 @@
+docker run -e KIE_CONTAINER_DEPLOYMENT='fhir-framinghamRules=com.vizuri.fhir:fhir-framinghamRules:1.0-SNAPSHOT' -e KIE_SERVER_USER=kieserver -e KIE_SERVER_PASSWORD=kieserver1! framingham-rules
